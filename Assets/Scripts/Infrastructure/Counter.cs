2f59dc639a2e06d5b824dd492410f47f78ff0ddb
AEONNIS <yogi333@yandex.ru>
2020-09-03 11:23:25 +0300

    Код для Counter и CounterIndicator

---
+++ Assets/Scripts/Infrastructure/Counter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using UnityEngine;
+
+namespace WhacAMole.Infrastructure
+{
+    [Serializable]
+    public class Counter
+    {
+        [SerializeField] private int _value = 0;
+        [SerializeField] private int _minLimiter = int.MinValue;
+        [SerializeField] private int _maxLmiter = int.MaxValue;
+
+        public event Action Decreased;
+        public event Action Increased;
+
+        public int Value => _value;
+
+        public int Decrease()
+        {
+            if (_value > _minLimiter)
+            {
+                --_value;
+                Decreased?.Invoke();
+            }
+
+            return _value;
+        }
+
+        public int Increase()
+        {
+            if (_value < _maxLmiter)
+            {
+                ++_value;
+                Increased?.Invoke();
+            }
+
+            return _value;
+        }
+    }
+}
--- Assets/Scripts/UI/CounterIndicator.cs
+++ Assets/Scripts/UI/CounterIndicator.cs
@@ -1,56 +1,52 @@
 ﻿using System;
 using UnityEngine;
 using UnityEngine.UI;
+using WhacAMole.Infrastructure;
 
 namespace WhacAMole.UI
 {
     public class CounterIndicator : MonoBehaviour
     {
-        [SerializeField] private Button _decreaseCounterButton;
-        [SerializeField] private Button _increaseCounterButton;
-        [SerializeField] private Text _counter;
+        [SerializeField] private Counter _counter;
+        [SerializeField] private Button _decreaseButton;
+        [SerializeField] private Button _increaseButton;
+        [SerializeField] private Text _indicator;
 
-        private Action _decreaseAction;
-        private Action _increaseAction;
-        private int _value = 0;
+        public event Action Decreased
+        {
+            add => _counter.Decreased += value;
+            remove => _counter.Decreased -= value;
+        }
+
+        public event Action Icreased
+        {
+            add => _counter.Increased += value;
+            remove => _counter.Decreased -= value;
+        }
 
         #region Unity
+        private void Awake()
+        {
+            SetIndicator(_counter.Value);
+        }
+
         private void OnEnable()
         {
-            _decreaseCounterButton.onClick.AddListener(Decrease);
-            _increaseCounterButton.onClick.AddListener(Increase);
+            _decreaseButton.onClick.AddListener(Decrease);
+            _increaseButton.onClick.AddListener(Increase);
         }
 
         private void OnDisable()
         {
-            _decreaseCounterButton.onClick.RemoveListener(Decrease);
-            _decreaseCounterButton.onClick.RemoveListener(Increase);
+            _decreaseButton.onClick.RemoveListener(Decrease);
+            _decreaseButton.onClick.RemoveListener(Increase);
         }
         #endregion
 
-        public void Init(int value, Action decreaseAction = null, Action increaseAction = null)
-        {
-            SetCounter(value);
-            _decreaseAction = decreaseAction;
-            _increaseAction = increaseAction;
-        }
+        private void Decrease() => SetIndicator(_counter.Decrease());
 
-        private void Decrease()
-        {
-            SetCounter(--_value);
-            _decreaseAction?.Invoke();
-        }
+        private void Increase() => SetIndicator(_counter.Increase());
 
-        private void Increase()
-        {
-            SetCounter(++_value);
-            _increaseAction?.Invoke();
-        }
-
-        private void SetCounter(int value)
-        {
-            _value = value;
-            _counter.text = value.ToString();
-        }
+        private void SetIndicator(int value) => _indicator.text = value.ToString();
     }
 }
